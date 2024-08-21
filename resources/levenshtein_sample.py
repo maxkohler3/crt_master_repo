06c0c477da750468ad1309f1a738d6b5e652298e
Max Kohler <mkohler@copado.com>
2024-08-21 11:32:31 -0400

    aaa

--- resources/levenshtein_sample.py
+++ resources/levenshtein_sample.py
@@ -1,7 +1,4 @@
 from Levenshtein import levenshtein_distance_weighted
 
-def calculate_levenshtein_distance(string1, string2):
-    """
-    Calculate the Levenshtein distance between two strings.
-    """
-    return distance(string1, string2)
+def get_levenshtein_distance(self, incoming_text, master_text):
+        return levenshtein_distance_weighted(incoming_text,master_text)
