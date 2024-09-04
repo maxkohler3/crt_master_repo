9bf47865fcd3687752d6342ab41bf86145a51c2a
Max Kohler <mkohler@copado.com>
2024-09-04 12:56:20 -0400

    Deleting levenshtein_sample.py via Copado Robotic Testing editor.

--- resources/levenshtein_sample.py
+++
@@ -1,4 +0,0 @@
-from Levenshtein import levenshtein_distance_weighted
-
-def get_levenshtein_distance(self, incoming_text, master_text):
-        return levenshtein_distance_weighted(incoming_text,master_text)
